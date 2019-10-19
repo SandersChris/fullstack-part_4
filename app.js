f553a6c1c5bf52acd97334776615474e1c01ce1a
Chris Sanders <christopherdksanders@gmail.com>
2019-10-19 16:10:17 -0700

    4.1 and 4.2 blog list refactoring

---
+++ app.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+console.log('connecting to', config.MONGODB_URI)
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
